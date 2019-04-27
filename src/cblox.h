b343586346d7a6039328166dcb91f4683164d985
Meqolo <45327591+Meqolo@users.noreply.github.com>
2019-04-27 14:50:06 +0100

    First commit
    Has libcurl, GetFriends function and a json library

---
+++ src/cblox.h
@@ -0,0 +1,18 @@
+#include <string>
+#include "json.h"
+#define CURL_STATICLIB
+
+using json = nlohmann::json;
+
+using namespace std;
+
+namespace cblox {
+	class User {
+	public:
+		static json GetFriends(int userId);
+	};
+	class Http {
+	public:
+		static string Get(string url);
+	};
+}
